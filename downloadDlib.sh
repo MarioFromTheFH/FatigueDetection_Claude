396525024f5f33e0b7275ed68136f1804ab9ac70
Mario J. Schwaiger <mario.schwaiger@v2c2.at>
2025-07-29 01:08:43 +0200

    Additional Files

---
+++ downloadDlib.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Download the 68-point facial landmark predictor
+wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+bunzip2 shape_predictor_68_face_landmarks.dat.bz2
